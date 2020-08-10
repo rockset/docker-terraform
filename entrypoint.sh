82b3e61fef536e2e8ba3c689da0a5f910a33961a
Martin Englund <pme@rockset.com>
2020-08-10 09:29:49 -0700

    always run terraform init

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
+set -e
+
 if [ -n "${TERRAFORM_DIR}" ]; then
   cd "${TERRAFORM_DIR}" || exit 1
 fi
 
-# run terraform init if it hasn't been called yet
-if [ ! -d ".terraform" ]; then
-  /aws-sm-env terraform init
-fi
+/aws-sm-env terraform init
 
 /aws-sm-env "$@"
